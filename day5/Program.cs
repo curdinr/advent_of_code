8803d321dda08d9a161ece6ed526b1f507df3e67
curdinr <curdinroffler@gmail.com>
2022-12-12 21:55:39 +0100

    begin day5

---
+++ day5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace day5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt"));
+
+            Console.WriteLine($"1: {PartOne(input)}");
+            //Console.WriteLine($"2: {PartTwo(input)}");
+        }
+
+        private static object PartOne(string[] input)
+        {
+            Stack<string> s1;
+        }
+
+        // private static object PartTwo(string[] input)
+        // {
+            
+        // }
+    }
+}
