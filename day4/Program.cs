a85bd07480b1b65d97ffc06432e92930c8bdb8b1
curdinr <curdinroffler@gmail.com>
2022-12-12 21:37:18 +0100

    add day 4

---
+++ day4/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace day4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt"));
+
+            Console.WriteLine($"1: {PartOne(input)}");
+            Console.WriteLine($"2: {PartTwo(input)}");
+        }
+
+private static object PartOne(string[] input)
+        {
+            int index = 0;
+            string regex = @"(?<g1>\d+)-(?<g2>\d+),(?<g3>\d+)-(?<g4>\d+)";
+            foreach (string line in input) 
+            {
+                Match match = Regex.Match(line, regex);
+                var startSectionOne  = Int32.Parse(match.Groups["g1"].Value);
+                var endSectionOne = Int32.Parse(match.Groups["g2"].Value);
+                var startSectionTwo = Int32.Parse(match.Groups["g3"].Value);
+                var endSectionTwo = Int32.Parse(match.Groups["g4"].Value);
+
+                if ((startSectionOne <= startSectionTwo && endSectionOne >= endSectionTwo) || (startSectionTwo <= startSectionOne && endSectionOne <= endSectionTwo))
+                    index++;
+            }
+
+            return index;
+        }
+
+        private static object PartTwo(string[] input)
+        {
+            int index = 0;
+            string regex = @"(?<g1>\d+)-(?<g2>\d+),(?<g3>\d+)-(?<g4>\d+)";
+            foreach (string line in input)
+            {
+                Match match = Regex.Match(line, regex);
+                var startSectionOne = Int32.Parse(match.Groups["g1"].Value);
+                var endSectionOne = Int32.Parse(match.Groups["g2"].Value);
+                var startSectionTwo = Int32.Parse(match.Groups["g3"].Value);
+                var endSectionTwo = Int32.Parse(match.Groups["g4"].Value);
+
+                if (Enumerable.Range(startSectionOne, endSectionOne - startSectionOne + 1).Any(x => x == startSectionTwo || x == endSectionTwo) || Enumerable.Range(startSectionTwo, endSectionTwo - startSectionTwo + 1).Any(x => x == startSectionOne || x == endSectionOne)) 
+                    index++;
+            }
+
+            return index;
+        }
+    }
+}
