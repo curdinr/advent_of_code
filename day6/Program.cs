48c4c9eb1855fa68128439cb895d67f0fe64d98b
curdinr <curdinroffler@gmail.com>
2022-12-15 07:36:28 +0100

    fix day 6

---
+++ day6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace day5
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "puzzleInput.txt"));
+
+            Console.WriteLine($"1: {SolvePuzzle(input, 4)}");
+            Console.WriteLine($"2: {SolvePuzzle(input, 14)}");
+        }
+
+        private static object SolvePuzzle(string input, int lenghtPart)
+        {
+            char[] chars = input.ToCharArray();
+            Queue<char> queue = new Queue<char>();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (queue.Count == lenghtPart)
+                {
+                    if (!queue.GroupBy(x => x).Any(g => g.Count() > 1))
+                    {
+                        return i;
+                    }
+
+                    queue.Dequeue();
+                }
+
+                queue.Enqueue(chars[i]);
+            }
+
+            return 0;
+        }
+    }
+}
